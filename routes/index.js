929769f6d6f207dbb644fd4b6786fba58d1aeaa0
mjeng <mjeng@ucsd.edu>
2016-02-18 19:58:44 +0000

    more analytics

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,19 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
-exports.view = function(req, res){
-	projects["grid"] = false;
-	res.render('index', projects);
-};
+ exports.view = function(req, res){
+ 	var random_num = Math.random();
+ 	console.log(random_num);
 
-exports.viewGrid = function(req, res) {
-	projects["grid"] = true;
-	res.render('index', projects);
-}
+ 	if (random_num >= 0) {
+ 		projects['grid'] = false;
+ 		res.render('index', projects);
+ 	} else {
+ 		res.redirect('/grid');
+ 	}
+ };
+
+ exports.viewGrid = function(req, res) {
+ 	projects["grid"] = true;
+ 	res.render('index', projects);
+ }
